2110b4e5cb28b521b11d05810773b6ae6c12cbc2
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2023-03-22 15:01:33 +0100

    Update plot_error_dew.py

--- assessingsolar/error_gallery/plot_error_dew.py
+++ assessingsolar/error_gallery/plot_error_dew.py
@@ -20,7 +20,6 @@ unventilated and unheated pyranometers.
 #   :alt: Image of two pyranometers, one with dew and one without.
 #   :width: 400
 #
-# sphinx_gallery_thumbnail_path = '../graphics/dew_pyranometers_dtu_20210112.jpg'
 #
 # Detecting dew
 # -------------
@@ -28,9 +27,13 @@ unventilated and unheated pyranometers.
 # and see if we can observe this pheonmena.
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import pvlib
+# sphinx_gallery_thumbnail_path = '../graphics/dew_pyranometers_dtu_20210112.jpg'
 
 filename = '../notebooks/data/solar_irradiance_dtu_2019.csv'
 df = pd.read_csv(filename, index_col=0, parse_dates=True)
+df.index = df.index.tz_localize('UTC')
 
 # Calculate Global Horizontal Irradiance (GHI) from Diffuse Horizontal
 # Irradiance (DHI) and Direct Normal Irradiance (DNI) using the closure
@@ -50,16 +53,31 @@ df.plot.scatter(x='GHI', y='GHI_calc', s=1, alpha=0.5, grid=True,
 
 # %%
 # A closer inspection of the data, let's us find a specific day where this
-# phenomena is pronounced.
-ax = df['2019-05-11 00':'2019-05-11 23'].plot.scatter(
+# phenomena is pronounced. As an example we'll look at May 11th 2019.
+df['2019-05-11 03':'2019-05-11 20'].plot.scatter(
     x='GHI', y='GHI_calc', s=1, grid=True, c='hourofday', cmap='plasma',
     sharex=False)
-ax.set_title('2019-05-11')
+plt.show()
 
 # %%
 # From the above plot, it's clear that this phenomena occurs in the morning.
 # Let's see if we can also see the issue in the raw measurements for the same
 # day:
-ax = df.loc['2019-05-11 00':'2019-05-11 23',
-            ['DNI', 'GHI_calc', 'GHI', 'DHI']].plot(grid=True, alpha=0.5)
-ax.set_title('2019-05-11')
+df.loc['2019-05-11 03':'2019-05-11 20',
+       ['DNI', 'GHI_calc', 'GHI', 'DHI']].plot(grid=True, alpha=0.5);
+
+# %%
+# Let's attempt to see if we can detect the dewy periods using a traditional
+# Kd vs. Kt plot. This type of plot is commonly used in quality assessment
+# of irradiance measurements.
+df['Kd'] = df['DHI'] / df['GHI']
+df['dni_extra'] = pvlib.irradiance.get_extra_radiation(df.index)
+df['ghi_extra'] = df['dni_extra'] * np.cos(np.deg2rad(df['zenith']))
+df['Kt'] = df['GHI'] / df['ghi_extra']
+
+df['2019-05-11 03':'2019-05-11 20'].plot.scatter(
+    x='Kt', y='Kd', grid=True, xlim=(0, 1.1), ylim=(0, 1.1), s=5,
+    c='hourofday', cmap='plasma', sharex=False)
+plt.show()
+
+#
