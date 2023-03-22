e2f01125d43d9fe94624572c2b4c20b8c4238060
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2023-03-22 14:00:41 +0100

    Add plot error dew

---
+++ assessingsolar/error_gallery/plot_error_dew.py
@@ -0,0 +1,13 @@
+"""
+Dew on radiometers
+==================
+
+Examples of generating sunpath diagrams.
+"""
+
+# %%
+# This example shows the irradiance measurements for a day with dew on the
+# instruments.
+#
+# .. image:: ../graphics/dew_pyranometers_dtu_20210112.jpg
+#   :alt: Image of two pyranometers, one with dew and one without.
